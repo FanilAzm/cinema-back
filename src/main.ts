f567abcd714f3d841594810663c22459e9478c74
FanilAzm <fanil.azmagulov@mail.ru>
2023-02-06 18:08:31 +0400

    backend cinema-app

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule)
+	const port = process.env.PORT || 4200
+	app.setGlobalPrefix('api')
+	app.enableCors()
+	await app.listen(port)
+}
+bootstrap()
